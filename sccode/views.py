d15b8519ee271e03727e5ef5033dcf7595b4eddc
Roshan <roshanmechoth@gmail.com>
2018-03-22 00:07:56 +0530

    modified

--- sccode/views.py
+++ sccode/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 import csv
 import json
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.http import JsonResponse
 
-# Create your views here.
 
 def create_sccode(request,sccode):
     csv_file ={}
@@ -13,12 +12,18 @@ def create_sccode(request,sccode):
         reader = csv.reader(file)
         reader.next()
         for SC_CODE in reader:
-            if sccode in SC_CODE[0]:
+            if sccode == SC_CODE[0]:
                 csv_file['SC_CODE']=SC_CODE[0]
                 csv_file['SC_NAME']=SC_CODE[1]
                 csv_file['OPEN']=SC_CODE[4]
                 csv_file['HIGH']=SC_CODE[5]
                 csv_file['LOW']=SC_CODE[6]
                 csv_file['CLOSE']=SC_CODE[7]
-        print csv_file
-    return HttpResponse(json.dumps(csv_file), content_type="application/json")
+                response_data=csv_file
+            else:
+                response_data={"code":"404", "message":"No data"}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+
+
